3cbf877ee5f1f9230af81b560913ec233845686e
Matheus Vasconcelos <marco-til1@hotmail.com>
2025-09-23 17:40:48 -0300

    Exercicos com estrutura de repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+// var contador = 50
+// while (contador >= 0){
+//     console.log(contador)
+//     contador--
+// }
+// var contador = 0
+// while (contador <= 50){
+//     console.log(contador)
+//     contador++
+// }
+
+var c = 1
+do {
+    console.log(c)
+    c++
+} while (c >= 50)
+
+    
+var c = 1 
+while (c <=6){
+    console.log('Passo ' + c)
+    c++
+}
---
+++ aula14/anbiente.js
@@ -0,0 +1,9 @@
+console.log('Vai começar...')
+for(c=1; c<=10; c++){
+    console.log('Passo ' + c)
+}
+console.log('Fim!')
+// for(c = 10; c >= 1; c--){
+//     console.log('Passo ' + c)
+// }
+ 
